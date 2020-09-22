72c673a6d920d7cfa28371bb8c0648251060d509
andrea <andreatv12@gmail.com>
2020-09-22 13:59:31 +0100

    very simple react project helloworld

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import logo from './logo.svg';
 import './App.css';
-import $ from 'jquery'; 
+import $ from 'jquery';
+import { useForm } from "react-hook-form";
+
+
 
 function App() {
-    const logic = () => {
-        console.log("hello")
-        var player="Elaine";
+    var playerStatus="n/a"
+
+    const {register, handleSubmit, errors} = useForm();
+
+    const onSubmit = data => {
+        var player = document.getElementById("playerCheck").value;
+        console.log("player: ",player)
         $.ajax({
             data:"QUERY="+player,
             url:'http://127.0.0.1:8080/query',
@@ -14,23 +22,32 @@ function App() {
             success:function(data)
             {
                 console.log("dataaaaa: ",data)
-
+                playerStatus="found!"
+                document.getElementById("playerStatus").value=playerStatus
             },
             error: function()
             {
                 //alert(player,' not found!');
                 console.log("not found: ",player)
+                playerStatus="not found"
+                document.getElementById("playerStatus").value=playerStatus
             }
         });
     }
     
-  return(
-      <div>
-      <h2>Hello</h2>
-      <button onClick={logic}>Hello</button>
-      </div>
-  )
+    return(
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <p>Enter a player name to check if she is in the database:</p>
+                <input id="playerCheck" type='text' placeholder="Elaine0"/>
+                <input type='submit'/>
+            </form>
+
+            <input id="playerStatus" value={playerStatus}/>
+        </div>
+    )
   
 }
-export default App; // Don’t forget to use export default!
+
+export default App;
 
